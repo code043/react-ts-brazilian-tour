aecc9c0262f78238cb6c8308250451aec222577d
code043 <procoder043@gmail.com>
2024-09-12 18:56:03 -0300

    Api com loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import { useState, useEffect } from "react";
 import Tours from "./components/tours-list/Tours";
 import { ToursData } from "./types/ToursList";
-import infos from "./database/infos.json";
 import "./index.css";
+import Loading from "./components/loading/Loading";
 
 function App() {
   const [tours, setTours] = useState<ToursData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const getTours = async () => {
+    setLoading(true);
+    try {
+      /**
+       * API Repository: https://github.com/code043/node-ts-api-brazilian-tour
+       */
+      const response = await fetch("https://tours-one-vert.vercel.app/tours");
+      const data = await response.json();
+      console.log(data);
+      setTours(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    setTours(infos);
+    getTours();
   }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
 
   return (
     <main>
---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="header">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
